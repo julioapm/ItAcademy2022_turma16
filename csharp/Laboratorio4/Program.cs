0e48c401231cdf085c696ebd17cb9a384498beb0
Júlio Machado <julioapm@gmail.com>
2022-07-20 15:31:57 -0300

    Implementação de Correntista

--- csharp/Laboratorio4/Program.cs
+++ csharp/Laboratorio4/Program.cs
@@ -3,3 +3,9 @@ ContaCorrente conta2 = new ContaCorrente(1);
 conta1.Depositar(100);
 conta1.Sacar(50);
 Console.WriteLine(conta1.Saldo);
+Console.WriteLine(conta1.DataCriacao.ToShortDateString());
+
+Correntista c1 = new Correntista {Nome="John Doe", Cpf="12345678901"};
+Console.WriteLine(c1.Nome);
+Console.WriteLine(c1.Cpf);
+
