06745cae6d38d35ef54854d1089122d9860880d5
Júlio Machado <julioapm@gmail.com>
2022-07-21 16:08:41 -0300

    Laboratorio4 com delegates e lambda

---
+++ csharp/Laboratorio3/Program.cs
@@ -0,0 +1,20 @@
+﻿List<string> listaStrings = new List<string>();
+listaStrings.Add("Um");
+listaStrings.Add("Hello");
+listaStrings.Add("World");
+Console.WriteLine(listaStrings[0]);
+//Console.WriteLine(listaStrings[3]);
+listaStrings[0] = "Alterado";
+Console.WriteLine(listaStrings[0]);
+Console.WriteLine(listaStrings.Count);
+
+/*
+foreach (var item in listaStrings)
+{
+    Console.WriteLine(item);
+}
+*/
+listaStrings.ForEach(Console.WriteLine);
+listaStrings.ForEach(item => Console.WriteLine(item.Length));
+var elemento = listaStrings.Find(item => item[0] == 'Z');
+Console.WriteLine(elemento);
